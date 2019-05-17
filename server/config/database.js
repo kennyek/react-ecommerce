9db546fe411fd104ffec8d3e625346de56b73921
Kenny Ek <ekkenny@hotmail.com>
2019-05-17 13:59:48 +0200

    Fix warnings on mongoose deprecations

--- server/config/database.js
+++ server/config/database.js
@@ -1,8 +1,10 @@
 const mongoose = require('mongoose');
 
+mongoose.set('useCreateIndex', true);
 mongoose.Promise = global.Promise;
 
 const { DATABASE } = process.env;
-const connection = mongoose.connect(DATABASE);
+const options = { useNewUrlParser: true };
+const connection = mongoose.connect(DATABASE, options);
 
 module.exports = connection;
